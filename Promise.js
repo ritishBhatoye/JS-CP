404e8bc1142def7231ca8c0c6a96beca284ba1ec
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-17 16:56:40 +0530

    JS_CP

---
+++ Promise.js
@@ -0,0 +1,14 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+let p=new Promise(function(resolve,rejct){
+    alert("Hi");
+    resolve(94)
+});
+
+setTimeout(function (){
+    console.log(p);
+},5000);
+
+
+
